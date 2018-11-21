4155265dec029c1b913d027988d9dbb03ac355d7
BogdanMateratskyy <bogdan.materatskyy@gmail.com>
2018-11-21 19:58:56 +0200

    Initial commit

---
+++ Practical_11/dbo/Tables/MovingAverage.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[MovingAverage] (
+    [SeriesReference] NVARCHAR (50)    NULL,
+    [Type]            NVARCHAR (50)    NULL,
+    [DataValue]       NVARCHAR (50)    NULL,
+    [LowerCI]         NVARCHAR (50)    NULL,
+    [UpperCI]         NVARCHAR (50)    NULL,
+    [Units]           NVARCHAR (50)    NULL,
+    [Indicator]       NVARCHAR (50)    NULL,
+    [Cause]           NVARCHAR (50)    NULL,
+    [Validation]      NVARCHAR (50)    NULL,
+    [Population]      NVARCHAR (50)    NULL,
+    [Age]             NVARCHAR (50)    NULL,
+    [Severity]        NVARCHAR (50)    NULL,
+    [Year]            BIGINT          NULL,
+    [Month]           INT             NULL,
+    [Day]             INT             NULL,
+    [Rate]            NUMERIC (38, 6) NULL
+);
+
